94dbbc0f11e4ec1d78d164c23616dac4a51e0ca4
chelseamharmon <cmh2228@columbia.edu>
2018-11-29 14:21:54 -0500

    Create extract_mid_image.sh

---
+++ ch_scripts/QA/extract_mid_image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sublist=$(<../subjRes4dPath.txt)
+for i in $sublist;
+  do
+  j=${i:20:3}
+  k=${i:29:1}
+
+  #extract first volume 
+  #fslsplit $i /danl/Harmon_dynCon/scripts/QA/$j #This also works but seperates into all volumes 
+  fslroi $i /danl/Harmon_dynCon/scripts/QA/$j$k 99 100  #this just gives us the first volume 
+done
+
+fslmerge -t res4d_std_vol99_mid 7*
